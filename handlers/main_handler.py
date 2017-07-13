9b6b4a748398f293524905806cff26c78640dc44
qdog437 <qjblackston@gmail.com>
2017-07-13 09:53:21 -0400

    debugging 1
     current_user = users.get_current_user()
    
    NameError: global name 'users' is not defined

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -12,3 +12,12 @@ class MainHandler(webapp2.RequestHandler):
         }
         template = jinja_env.env.get_template('templates/tmpl.html')
         self.response.out.write(template.render(html_params))
+
+        current_user = users.get_current_user()
+        logging.info(users.get_current_user())
+        logging.info(users.create_login_url("/rachel"))
+        self.response.out.write('<h1> This is different</h1>')
+        user_comments_query = Comment.query(Comment.author == current_user.email())
+        user_comments = user_comments_query.fetch()
+        logging.info(len(user_comments))
+        comments =  Comment.query().fetch()
