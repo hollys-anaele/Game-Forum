8db58dcd6937e9a0773304ae99c99519cdc166b3
stovegit <stovebruh@gmail.com>
2017-07-18 08:59:24 -0400

    sagsd

--- handlers/ps4_handler.py
+++ handlers/ps4_handler.py
@@ -6,46 +6,47 @@ import os
 
 from google.appengine.ext import ndb
                      
+
 class Comment(ndb.Model):
     author = ndb.StringProperty()
-    contents = ndb.StringProperty()
-    animal = ndb.StringProperty()
-    
+    user_comment = ndb.StringProperty()
+    game_name = ndb.StringProperty()
+
+                     
 class PS4Handler(webapp2.RequestHandler):
+
+
     def get(self):
+   #     "title": "PS4 Discussion Board",
     	logging.info("PS4Handler")
         comments = Comment.query().fetch()
         comment_str = ""
         for comment in comments:
             comment_str += "<div>"
-            comment_str += "<h3>" + "comment by " + comment.author + ":" "</h3>"
-            comment_str += "<p>" + comment.contents + "</p>"
-            comment_str += "<p>" + str(comment.animal) + "</p>" #new line
+            comment_str += "<h3>" + "Post by " + comment.author + ":" "</h3>"
+            comment_str += "<p>" + str(comment.user_comment) + "</p>"
+            comment_str += "<p>" + str(comment.game_name) + "</p>" 
             comment_str += "</div>"
        
         thing = {
-            "title": "PS4 Discussion Board",
             "html_comments": comment_str
         }
         template = jinja_env.env.get_template("templates/tmpl.html")
         self.response.out.write(template.render(thing))
 
+
+
        
     def post(self):
-        r_author = self.request.get("forum1_author")
-        r_comment = self.request.get("forum1_comment")
+        r_author = self.request.get("author")
+        r_comment = self.request.get("comment")
+        r_game = self.request.get("game")
         
 
-        new_comment = Comment(author=r_author, contents=r_comment)
+        new_comment = Comment(author=r_author, user_comment=r_comment, game_name=r_game)
         new_comment.put()
         
         self.redirect("/ps4")
         
-            
-        thing = {
-            "html_comments": comment_str
-        }
-        template = jinja_env.get_template("templates/comments.html")
-        self.response.out.write(template.render(thing))
-        
-        
+
+
