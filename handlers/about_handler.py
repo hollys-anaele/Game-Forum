7dafd78872f775da107b6d8f0373af28b5cfdfad
qdog437 <qjblackston@gmail.com>
2017-07-19 11:00:18 -0400

    about us update

--- handlers/about_handler.py
+++ handlers/about_handler.py
@@ -10,14 +10,7 @@ class AboutHandler(webapp2.RequestHandler): #the about page
         html_params = {
             "title": "About",
             "content": "This is the About page. Our website is dedicated to provide a forum for individuals to discuss their favourite game consoles and video games freely.<br>We host a multitude of categories, all regarding the video game industry. The main consoles hosted here are Playstation 4, XBox One, and a combination of both. </br><br>The creators of this website, Arcade Crawlers, are students of Google highly dedicated to attaining a career in Computer Science. Quincy Blackston, Steven Hodge, <br> and Hollys Anaele, are all proud to have made this page for your convenience.</br><br>Thank you, and happy posting!</br>"
-            
-
-			
-
-
-
- 
-
+           
         }
 
         template = jinja_env.env.get_template('templates/pictures.html')
