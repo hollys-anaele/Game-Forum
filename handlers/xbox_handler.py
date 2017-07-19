f66f606bca1c65568399f1ec0e2b8fa332cca9d9
qdog437 <qjblackston@gmail.com>
2017-07-19 08:58:49 -0400

    xbox update

--- handlers/xbox_handler.py
+++ handlers/xbox_handler.py
@@ -5,14 +5,17 @@ import webapp2
 import os
 
 from google.appengine.ext import ndb
+from google.appengine.api import users
 from models import comment_model
 
 
     
 class XboxHandler(webapp2.RequestHandler):
     def get(self):
-
-    	logging.info("XboxHandler")
+        logging.info("XboxHandler")
+        logging.info(users.get_current_user())
+        logging.info(users.create_login_url('/xbox'))
+    	
         comments = comment_model.Comment.query(comment_model.Comment.platform == "xbox").fetch()
         comment_str = ""
         for comment in comments:
@@ -22,22 +25,25 @@ class XboxHandler(webapp2.RequestHandler):
             comment_str += "<p>" + str(comment.user_comment) + "</p>"
             comment_str += "</div>"
        
-        thing = {
-            "html_comments": comment_str
-        }
         template = jinja_env.env.get_template("templates/tmpl.html")
         self.response.out.write(template.render(thing))
-
-
-
-       
+        thing = {
+            "html_comments": comment_str
+            "html_login_url": users.create_login_url('/xbox')
+        }
+        
+               
     def post(self):
+        user = users.get_current_user()
+        if user != None:
+            henry["user_email"] = user.email()   
+
         r_author = self.request.get("form_author")
         r_comment = self.request.get("form_comment")
         r_game = self.request.get("form_game")
         
 
-        new_comment = comment_model.Comment(author=r_author, user_comment=r_comment, game_name=r_game)
+        new_comment = comment_model.Comment(author=user.email(), user_comment=r_comment, game_name=r_game)
         new_comment.put()
         
         self.redirect("/xbox")
